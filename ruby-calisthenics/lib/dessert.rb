fcd8c3e32bd4fc4b6545f81327622350c904aa43
Saujanya Mani <smani@unomaha.edu>
2017-06-04 23:46:44 +0000

    Exercise-3

--- ruby-calisthenics/lib/dessert.rb
+++ ruby-calisthenics/lib/dessert.rb
@@ -1,17 +1,56 @@
 class Dessert
   def initialize(name, calories)
     # your code here
+    @name = name
+		@calories = calories
   end
+  #setter method
+	def name=(newname)
+		@name = newname
+	end
+	#getter method
+	def name
+		@name
+	end
+
+	def calories
+		@calories
+	end
+
+	#setter method
+	def calories=(newcals)
+		@calories = newcals
+	end
+	
   def healthy?
     # your code here
+    @calories < 200
   end
   def delicious?
     # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
     # your code here
+    @flavor = flavor
   end
+  def flavor
+		@flavor
+	end
+	
+	def flavor=(newflavor)
+		@flavor = newflavor
+  	end
+  
+	def delicious?
+    	# YOUR CODE HERE
+		if self.flavor == "black licorice" 
+			return false
+		else
+			return true
+		end
+  	end
 end
