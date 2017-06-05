e1859d1dd62a6491884aa757cb4632eab0b2c7ed
Saujanya Mani <smani@unomaha.edu>
2017-06-05 14:34:17 +0000

    Exercise-3

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -5,31 +5,22 @@ class Class
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
       # YOUR CODE HERE
-      def {attr_name}=(attr_name)
-			# a setter for whatever attr_name is 
-			@attr_name = attr_name
-			#history functionality
-			if @{attr_name}_history
-				@{attr_name}_history << attr_name #puts it into the history array of this func
-			else
-				#a previously not seen attr make an array and add it to it
-				@{attr_name}_history = Array.new
-				
-				@{attr_name}_history << nil # added this as the grader wanted first attr to be a nil ??
-				@{attr_name}_history << attr_name
-			end 
-		end
-		class Foo
-	#metaprogramming 
-    attr_accessor_with_history :bar
-end
-
-f = Foo.new     # => #<Foo:0x127e678>
-puts f.bar = 3       # => 3
-puts f.bar = :wowzo  # => :wowzo
-puts f.bar = 'boo!'  # => 'boo!'
-puts f.bar_history   # => [nil, 3, :wowzo, 'boo!']
-
+        def #{attr_name}=(val)
+                          if @#{attr_name}_history.nil?
+                              @#{attr_name}_history = [nil]
+                          end
+                        @#{attr_name}_history.push(val)
+                        @#{attr_name}=val
+                      end
     }
   end
 end
+class Check
+    attr_accessor_with_history :bar
+end
+
+f = Check.new     
+puts f.bar = 7      
+puts f.bar = :HalaMadrid!  
+puts f.bar = 'Vamos!' 
+puts f.bar_history 
